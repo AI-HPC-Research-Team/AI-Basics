cc3566c4d1789441dece926b4309d71795e62d6a
zt li <lizt@pcl.ac.cn>
2025-11-21 17:13:29 +0800

    add api key

---
+++ prepare_your_api_key.sh
@@ -0,0 +1,8 @@
+# wandb api key
+# get api key from https://wandb.ai/quickstart/
+export WANDB_API_KEY="your_api_key_here"
+
+# huggingface api key
+# get api key from https://huggingface.co/settings/tokens
+# needed when notenook login
+export HUGGINGFACE_API_KEY="your_api_key_here"  
